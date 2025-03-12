b8683fb44c933d4db645c5644e29865a565e4782
Rajesh <rk6289943975@gmail.com>
2025-03-13 00:42:02 +0530

    Time: 1 ms (75.59%) | Memory: 46 MB (8.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -24,35 +24,66 @@ class Solution {
             }
         }
         */
-        boolean row[] = new boolean[m];
-        boolean col[] = new boolean[n];
+        boolean zeroRow= false;
+        boolean zeroCol = false;
+
+        //check in zeroRow
+        for(int j =0 ; j < n;j++){
+            if(arr[0][j]==0){
+                zeroRow = true;
+                break;
+            }
+        }
+        
+        //check in zeroCol
+        for(int i =0 ; i < m;i++){
+            if(arr[i][0]==0){
+                zeroCol = true;
+                break;
+            }
+        }
+
         //marking the particular row and column 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
                 if(arr[i][j]==0){
-                    row[i]=true;
-                    col[j]=true;
+                    arr[i][0]=0;
+                    arr[0][j]=0;
                 }
             }
         }
 
         //set the true row to 0
-        for(int i = 0; i < m; i++){
-            if(row[i]==true){//set ith row to zero
-                for(int j = 0;j < n; j++){
+        for(int i = 1; i < m; i++){
+            if(arr[i][0]==0){//set ith row to zero
+                for(int j = 1;j < n; j++){
                     arr[i][j]=0;
                 }
             }
         }
         
         //set the true col to 0
-        for(int j = 0; j < n; j++){
-            if(col[j]==true){//set ith col to zero
-                for(int i = 0;i < m; i++){
+        for(int j = 1; j < n; j++){
+            if(arr[0][j]==0){//set ith col to zero
+                for(int i = 1;i < m; i++){
                     arr[i][j]=0;
                 }
             }
         }
+
+        //set 0th row to zero
+        if(zeroRow==true){
+            for(int j =0 ; j < n;j++){
+            arr[0][j]=0;
+            }
+        }
+        //set 0th col to zero
+        if(zeroCol==true){
+            for(int i =0 ; i < m;i++){
+            arr[i][0]=0;
+            }
+        }
+
 
     }
 }
