0c3be4b08053f9fa33bf8891ec31c2b5be7d73e2
Rajesh <rk6289943975@gmail.com>
2025-04-16 02:34:46 +0530

    Time: 3 ms (22.94%) | Memory: 46.2 MB (32.24%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nZero = 0; // Pointer to track position for next non-zero element
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) { // Agar current element zero nahi hai
-                if (i != nZero) { // Agar current index aur target index alag hai, tabhi swap karna hai
-                    // Swap current non-zero element to the correct position
-                    int temp = nums[i];
-                    nums[i] = nums[nZero];
-                    nums[nZero] = temp;
-                }
-                nZero++; // Agla non-zero element next position pe aayega
+        int nZero=0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i] != 0){ //coz array first index can be zero so we not apply arr[i]==0
+                if(i !=nZero){ //if current i is not nonzero means its zero so find zero
+                int temp = nums[i];//zero mila to non zero mai pass kar do
+                nums[i]=nums[nZero];
+                nums[nZero]=temp;
             }
+            nZero++;// Agla non-zero element next position pe aayega
+            }
+           
         }
     }
 }
