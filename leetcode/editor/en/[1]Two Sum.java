cf7617e4e76ab275d37b04079c38ed2cd4e8b117
rajesh-kayal-dev <rk6289943975@gmail.com>
2025-05-18 02:18:58 +0530

    [1]Two Sum.java solved

--- leetcode/editor/en/[1]Two Sum.java
+++ leetcode/editor/en/[1]Two Sum.java
@@ -1,8 +1,16 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-                }
+public int[] twoSum(int[] nums, int target) {
+    int n = nums.length;
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] + nums[j] == target) {
+                return new int[]{i, j};
             }
         }
     }
+    return new int[]{}; // No solution found
+}
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
