76dd98dd1ba2447d93f877a127eddb15b38f79d6
rajesh-kayal-dev <rk6289943975@gmail.com>
2025-05-18 02:04:50 +0530

    Add: Solved 'Two Sum' LeetCode problem

---
+++ leetcode/editor/en/[1]Two Sum.java
@@ -0,0 +1,65 @@
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than 
+//O(n²)
+// time complexity?
+//
+// Related Topics Array Hash Table 👍 61584 👎 2223
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
