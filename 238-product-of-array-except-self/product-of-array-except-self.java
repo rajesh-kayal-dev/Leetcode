94cb1e57475c624afb631cd3f57a99659e9bffe2
Rajesh <rk6289943975@gmail.com>
2025-06-12 09:37:31 +0530

    Time: 2 ms (89.12%) | Memory: 55.3 MB (87.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,30 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int ans []= new int[n];
-        //prefix
-        ans[0]=1;
-        for(int i = 1;i<n;i++){
-            ans[i]=ans[i-1] * nums[i-1];
+        int [] result = new int[n];
+
+        //nums=[1,2,3,4]
+
+        //result[1] = result[0] * nums[0] 
+        //1*1=1,1*2=2,2*3=6
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
         }
-        int suffix =1;
-        for(int j =n-1;j>=0;j--){
-            ans[j]=ans[j] * suffix;
-            suffix = suffix * nums[j];
+
+        //result=[1,1,2,6]
+        int right = 1;
+
+        for(int i = n-1;i >= 0; i-- ){
+            result[i] = result[i] * right;
+            right = right * nums[i];
         }
-        return ans;
+        //n=3>0 
+           //result[3]= 6 * 1 = 6, right = 1 * 4 = 4(right)
+                // [2]=2 * 4 =8, right = 4 * 3 = 12
+                // [1]=1 * 12 =12, right = 12 * 2 = 24
+                // [0]=1 * 24 =24, right = 24 * 1 = 24
+
+        return result;
     }
-}
+}  
