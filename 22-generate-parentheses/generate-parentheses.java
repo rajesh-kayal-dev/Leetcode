ae3afe040f5605407bc725378395d40bb3b303f6
Rajesh <rk6289943975@gmail.com>
2025-06-19 10:56:02 +0530

    Time: 2 ms (47.17%) | Memory: 43.4 MB (47.00%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generate(result,"",0,0,n);
+        return result;
+    }
+    public void generate(List<String> result, String current,int open,int close,int max){
+        //Base case: if current is complete add into result
+        if(current.length() == max * 2){ //n=3 ((())) 3* 2 -> 6 cahars
+        result.add(current);
+        }
+
+        if(open < max){
+            generate(result,current + "(", open + 1, close,max);
+        }
+
+        if(close < open){
+            generate(result, current + ")", open , close + 1,max);
+        }
+    }
+}
