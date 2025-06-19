9d9dd04ff616103a106fe453bf73bb91fe1d8eff
Rajesh <rk6289943975@gmail.com>
2025-06-19 23:24:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (52.59%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -14,11 +14,11 @@ class Solution {
         ListNode curr = head;
 
         while(curr != null){
-            ListNode next = curr.next;//2
-            curr.next= prev;//null
-            prev = curr;//1
-            curr = next;//2
+            ListNode next = curr.next;  // Step 1: next = 2
+            curr.next = prev;           // Step 2: 1.next = null (reverse)
+            prev = curr;                // Step 3: prev = 1
+            curr = next;                // Step 4: curr = 2
         }
-        return prev;
+        return prev; // points to 4 → 3 → 2 → 1 → null
     }
 }
