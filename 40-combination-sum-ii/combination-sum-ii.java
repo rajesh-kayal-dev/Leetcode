6d691c92bd1ee7f6fe1178045b60a433d3bfadd0
Rajesh <rk6289943975@gmail.com>
2025-07-03 12:05:17 +0530

    Time: 2 ms (99.66%) | Memory: 43.5 MB (35.85%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(0, nums, target,new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start,int[] nums,int target,List<Integer> current, List<List<Integer>> result){
+        if(target == 0){
+            result.add(new ArrayList<>(current));
+        }
+        for(int i = start;i < nums.length; i++){
+        if(i > start && nums[i] == nums[i -1]) continue;
+        if(nums[i] > target) break;
+        current.add(nums[i]);
+        backtrack(i + 1, nums , target - nums[i],current, result);
+        current.remove(current.size() - 1);
+        }
+
+    }
+
+}
