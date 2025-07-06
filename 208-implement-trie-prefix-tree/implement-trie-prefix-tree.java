80810c858c4c990586c2f9adcabea55c6751bd99
Rajesh <rk6289943975@gmail.com>
2025-07-06 10:42:04 +0530

    Time: 35 ms (61.33%) | Memory: 55.4 MB (70.68%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,40 +1,40 @@
 class Trie {
-
-    private class TriseNode{
-         TriseNode[] children = new TriseNode[26];  //26 lowercase letter
-         boolean isEndOfWord = false;
+    private class TrieNode{
+        TrieNode[] children = new TrieNode[26]; //a- z
+        boolean isEndOfWord = false;
     }
+    
+    private final TrieNode root ;
 
-    private final TriseNode root;
-
-    public Trie(){
-        root = new TriseNode();
+    public Trie() {
+        root = new TrieNode();
     }
     
     public void insert(String word) {
-        TriseNode node = root;
+        TrieNode node = root;
         for(char ch : word.toCharArray()){
             int index = ch - 'a';
             if(node.children[index] == null) {
-                node.children[index] = new TriseNode(); // create node if missing
+                node.children[index] = new TrieNode();
             }
-           node = node.children[index];
+          node = node.children[index];
         }
         node.isEndOfWord = true;
     }
     
     public boolean search(String word) {
-        TriseNode node = root;
+         TrieNode node = root;
         for(char ch : word.toCharArray()){
             int index = ch - 'a';
             if(node.children[index] == null) return false;
            node = node.children[index];
         }
         return node.isEndOfWord;
+        
     }
     
     public boolean startsWith(String prefix) {
-        TriseNode node = root;
+         TrieNode node = root;
         for(char ch : prefix.toCharArray()){
             int index = ch - 'a';
             if(node.children[index] == null) return false;
