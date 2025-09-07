c8f32f5a69e3a0a2465751efa1d6f49b50c739a9
Rajesh <rk6289943975@gmail.com>
2025-09-07 20:25:45 +0530

    Time: 7 ms (56.36%), Space: 20.6 MB (34.55%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function fib($n) {
+        if($n == 0 ) return 0;
+        if($n == 1 ) return 1;
+        $a = 0;
+        $b = 1;
+
+        for($i = 2; $i<= $n;$i++){
+            $c = $a + $b;
+            $a = $b;
+            $b = $c;
+        }
+        return $b;
+        
+    }
+}
