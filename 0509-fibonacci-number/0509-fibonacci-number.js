cd06a7f9b79a963b32f76c5011ef57448baec36a
Rajesh <rk6289943975@gmail.com>
2026-02-10 21:18:58 +0530

    Time: 38 ms (90.45%), Space: 54 MB (41.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,11 +3,15 @@
  * @return {number}
  */
 var fib = function(n) {
-   let arr = [0, 1];
+   if(n === 0 ) return 0;
+   if ( n == 1) return 1;
+   let a = 0;
+   let b =1;
 
-    for (let i = 2; i <= n; i++) {
-        arr.push(arr[i - 2] + arr[i - 1]);
-
-    }
-    return arr[n]; //[0,1] <-(0+1) 3, (1+3) 5
+   for(let i = 2 ;i <=n ;i++){
+    let c = a + b;
+    a=b;
+    b=c;
+   }
+   return b;
 };
