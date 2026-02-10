ede0400c2d898505352a89ed8bd2e83ac5b7f408
Rajesh <rk6289943975@gmail.com>
2026-02-10 20:16:43 +0530

    Time: 3 ms (48.81%), Space: 62.9 MB (72.27%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+
+    while(left < right){
+        let currentHeight = Math.min(height[left], height[right]);
+        let currentWidth = right - left;
+        let currentArea = currentWidth * currentHeight;
+        maxWater = Math.max(maxWater, currentArea);
+
+        if(height[left] < height[right]) left++;
+        else right--;
+    }
+    return maxWater;
+};
