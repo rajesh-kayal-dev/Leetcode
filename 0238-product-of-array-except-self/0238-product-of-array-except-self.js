767a4fb280bdb30d4d49bacece342856bc0329d8
Rajesh <rk6289943975@gmail.com>
2026-02-10 19:17:59 +0530

    Time: 5 ms (76.56%), Space: 69.6 MB (74.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -7,18 +7,17 @@ var productExceptSelf = function(nums) {
     let n = nums.length;
     let result = [];
 
-    // Calculate prefixes and store in result array
-    let prefixProduct = 1;
-    for (let i = 0; i < n; i++) {
-        result[i] = prefixProduct;
-        prefixProduct *= nums[i];
+    let prefix = 1;
+    for(let i = 0 ;i < n; i++){
+        result[i] = prefix;
+        prefix *= nums[i];
     }
-    
-    // Calculate suffixes and multiply with existing prefixes in the result array
-    let suffixProduct = 1;
-    for (let i = n - 1; i >= 0; i--) {
-        result[i] *= suffixProduct;
-        suffixProduct *= nums[i];
+
+
+    let suffix = 1;
+    for(let i = n -1; i>= 0;i--){
+        result[i] *= suffix;
+        suffix *= nums[i];
     }
 
     return result;
