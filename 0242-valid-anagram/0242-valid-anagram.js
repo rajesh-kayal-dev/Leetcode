fa63d7b99d80c6e6eb388e85b1156e230e188434
Rajesh <rk6289943975@gmail.com>
2026-02-10 18:39:13 +0530

    Time: 12 ms (70.53%), Space: 55.7 MB (77.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,10 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length != t.length) return false;
+    if(s.length !== t.length ) return false;
 
-    let sSort = s.split('').sort().join('');
-    let tSort = t.split('').sort().join('');
+    let map = new Map();
 
-    return sSort === tSort
+    for(let i = 0; i < s.length; i++){
+        map.set(s[i], (map.get(s[i]) || 0) +1)
+        map.set(t[i], (map.get(t[i]) || 0) -1)
+    }
+
+    for(let val of map.values()){
+        if(val !== 0 ) return false;
+    }
+    return true;
 };
