c762142fef8c76b5eabd0e238399427145e85fcf
Rajesh <rk6289943975@gmail.com>
2026-02-12 21:03:21 +0530

    Time: 10 ms (9.1%), Space: 46.9 MB (6.38%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int[] res = new int[set.size()];
+        int idx =0;
+
+        for(int i : set){
+            res[idx++] = i;
+        }
+        Arrays.sort(res);
+
+        if(res.length < 3){
+            return res[res.length -1];
+        }
+
+        return res[res.length-3];
+    }
+}
