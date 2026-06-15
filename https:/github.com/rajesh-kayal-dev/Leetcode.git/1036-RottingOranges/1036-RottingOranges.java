ac63a36ccb829492b2bfdc3378cb9a267ab9ee4a
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:36 +0530

    Create 1036-RottingOranges

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/1036-RottingOranges/1036-RottingOranges.java
@@ -0,0 +1,44 @@
+// Last updated: 6/15/2026, 9:59:36 AM
+class Solution {
+    public int orangesRotting(int[][] grid) {
+         if(grid == null || grid.length==0) return -1;
+
+
+        Queue<int[]> queue = new LinkedList<>();
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int fresh = 0;
+        for(int i = 0;i < rows;i++){
+            for(int j = 0;j < cols;j++){
+                if(grid[i][j]==2){
+                    queue.offer(new int[]{i,j});
+                }else if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
+        }
+        int minutes = 0;
+        int[][] directions ={{-1,0},{1,0},{0,-1},{0,1}};
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            boolean rotedThisMinutes = false;
+            for(int i = 0;i< size;i++){
+                int[] current = queue.poll();
+                int row = current[0], col = current[1];
+                for(int[] dir : directions){
+                    int r = row + dir[0];
+                    int c = col + dir[1];
+                    if(r >= 0 && c >= 0 && r < rows && c < cols && grid[r][c] == 1){
+                        grid[r][c] = 2;
+                        queue.offer(new int[]{r,c});
+                        fresh--;
+                        rotedThisMinutes = true;
+                    }
+                }
+            }
+            if(rotedThisMinutes) minutes++;
+            
+        }
+        return fresh == 0 ? minutes : -1;
+    }
+}
