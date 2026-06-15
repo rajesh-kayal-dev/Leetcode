b6f5bb311ca3b4b1b166c53339619ef2b5536904
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:32 +0530

    Create 1603-RunningSumOf1dArray

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.java
@@ -0,0 +1,10 @@
+// Last updated: 6/15/2026, 9:59:32 AM
+class Solution {
+    public int[] runningSum(int[] nums) {
+
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
+        return nums;
+    }
+}
