49fbc78ba9723996f3ab6c7784c14b59a476e9ef
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:01:51 +0530

    Create 146-LruCache

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/146-LruCache/146-LruCache.java
@@ -0,0 +1,27 @@
+// Last updated: 6/15/2026, 10:01:51 AM
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+      private int capacity;
+    public LRUCache(int capacity) {
+        super(capacity, 0.7f, true);
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
+        return size() > capacity;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
