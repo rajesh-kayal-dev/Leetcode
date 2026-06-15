f1f27772b94cce74520ab18b68086087ef2b2859
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:09 +0530

    Create 2053-CheckIfAllCharactersHaveEqualNumberOfOccurrences

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/2053-CheckIfAllCharactersHaveEqualNumberOfOccurrences/2053-CheckIfAllCharactersHaveEqualNumberOfOccurrences.js
@@ -0,0 +1,27 @@
+// Last updated: 6/15/2026, 9:59:09 AM
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areOccurrencesEqual = function(s) {
+    let arr = Array(128).fill(0);
+
+    for(let i = 0; i < s.length;i++){
+        let idx = s.charCodeAt(i);
+        arr[idx] += 1;
+    }
+    
+ 
+
+    let freq = new Set();
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            freq.add(arr[i]);
+        }
+    }
+
+    // If all frequencies are same, set size = 1
+    return freq.size === 1;
+
+
+};
