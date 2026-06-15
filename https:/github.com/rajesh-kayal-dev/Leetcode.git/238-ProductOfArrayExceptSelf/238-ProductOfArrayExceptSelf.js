dc48130d5983d275eed45ec52dc74d4dff760ed7
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:01:10 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.js
@@ -0,0 +1,25 @@
+// Last updated: 6/15/2026, 10:01:10 AM
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+    let n = nums.length;
+    let result = [];
+
+    let prefix = 1;
+    for(let i = 0 ;i < n; i++){
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+
+
+    let suffix = 1;
+    for(let i = n -1; i>= 0;i--){
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+};
