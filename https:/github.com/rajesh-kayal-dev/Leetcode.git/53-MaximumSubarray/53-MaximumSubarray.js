e194bc82fca03e845b411a9b3e2c715b0f0db458
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:03:02 +0530

    Create 53-MaximumSubarray

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/53-MaximumSubarray/53-MaximumSubarray.js
@@ -0,0 +1,17 @@
+// Last updated: 6/15/2026, 10:03:01 AM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let sum = 0;
+    let max = -Infinity;
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i];
+        max = Math.max(max, sum);
+
+        if(sum < 0 ) sum = 0;
+    }
+    return max;
+};
