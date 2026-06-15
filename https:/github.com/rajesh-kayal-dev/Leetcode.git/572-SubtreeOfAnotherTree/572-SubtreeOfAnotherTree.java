a6392d391df45dbe06674f37124a2fd6b129c9df
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:21 +0530

    Create 572-SubtreeOfAnotherTree

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.java
@@ -0,0 +1,35 @@
+// Last updated: 6/15/2026, 10:00:20 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+
+        if(isSameTree(root, subRoot)) return true;
+
+        //check left and right ssubTree
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q){
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
