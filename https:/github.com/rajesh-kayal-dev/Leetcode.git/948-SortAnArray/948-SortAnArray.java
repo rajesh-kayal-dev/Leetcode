2b5bc5b65edddccfc19169132b3da8d9808b672c
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:42 +0530

    Create 948-SortAnArray

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/948-SortAnArray/948-SortAnArray.java
@@ -0,0 +1,7 @@
+// Last updated: 6/15/2026, 9:59:42 AM
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);  // Dual-Pivot QuickSort for primitives
+        return nums;
+    }
+}
