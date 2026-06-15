03c42ffaa758ae528edf13b71efdde8734d69b8c
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:02:48 +0530

    Create 69-Sqrtx

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,24 @@
+// Last updated: 6/15/2026, 10:02:48 AM
+class Solution {
+    public int mySqrt(int n) {
+        	if (n==0|| n==1) 
+			return n;
+
+	int low = 0, high=n/2;
+	int ans=0;
+
+	while(low <= high){
+		int mid = low + (high - low)/2;
+		long squre =(long) mid*mid;
+		if (squre == n) {
+			return mid;
+		}else if (squre<n) {
+			ans=mid;
+			low=mid+1;//search right
+		}else{
+			high=mid -1;//search left
+		}
+	}
+	return ans;
+    }
+}
