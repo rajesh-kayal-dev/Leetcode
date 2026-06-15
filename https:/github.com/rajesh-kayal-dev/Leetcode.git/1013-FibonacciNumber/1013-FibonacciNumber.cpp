873c09e5583207060df9c24729e092195f90715d
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:10 +0530

    Create 1013-FibonacciNumber

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/15/2026, 10:00:10 AM
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 ) return 0;
+        if(n == 1 ) return 1;
+        int a = 0, b = 1;
+
+        for(int i = 2; i<= n;i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
