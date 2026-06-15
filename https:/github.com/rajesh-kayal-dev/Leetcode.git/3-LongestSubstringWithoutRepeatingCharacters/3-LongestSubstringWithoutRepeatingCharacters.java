326b84fe169fdd131afd6b027f5f1eb8bfd978b4
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:03:46 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,24 @@
+// Last updated: 6/15/2026, 10:03:46 AM
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int right = 0;
+        int left = 0;
+        int maxLen= 0;
+
+        HashSet<Character> set = new HashSet<>();
+
+    for(right = 0 ; right < s.length();right++){
+
+        //check duplicate and remove left move left
+        while(set.contains(s.charAt(right))){
+            set.remove(s.charAt(left));
+            left++;
+        }
+        set.add(s.charAt(right));
+        maxLen=Math.max(maxLen, right - left + 1);
+        //right 2 - left 0 + 1 = 3 (a,b,c)
+
+    }
+    return maxLen;
+    }
+}
