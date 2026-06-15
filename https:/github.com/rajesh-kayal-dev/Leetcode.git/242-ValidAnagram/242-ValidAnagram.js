be64548d8c5fe808ff693ae728ffeac4c258ef97
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:01:03 +0530

    Create 242-ValidAnagram

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/242-ValidAnagram/242-ValidAnagram.js
@@ -0,0 +1,21 @@
+// Last updated: 6/15/2026, 10:01:03 AM
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length ) return false;
+
+    let map = new Map();
+
+    for(let i = 0; i < s.length; i++){
+        map.set(s[i], (map.get(s[i]) || 0) +1)
+        map.set(t[i], (map.get(t[i]) || 0) -1)
+    }
+
+    for(let val of map.values()){
+        if(val !== 0 ) return false;
+    }
+    return true;
+};
