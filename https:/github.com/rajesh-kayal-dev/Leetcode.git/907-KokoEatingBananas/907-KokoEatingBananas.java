dc760f95a772b988858cd2d41c691006399b052f
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:45 +0530

    Create 907-KokoEatingBananas

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,37 @@
+// Last updated: 6/15/2026, 9:59:44 AM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        //find max
+        int max = 0;
+        for(int pile : piles){
+            max = Math.max(max, pile);
+        }
+
+        //apply binary search
+        int low = 1;
+        int high = max;
+        int result = high;
+        while(low <= high){
+            int mid = low + (high - low) /2;
+            //calculate total hours
+
+            int TotalHour = totalHours(piles, mid);
+
+            if(TotalHour <= h){
+                result = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    private int totalHours(int piles[], int k){
+        int hours = 0;
+        for(int pile : piles){
+            hours += Math.ceil((double)pile / k);
+        }
+        return hours;
+    }
+}
