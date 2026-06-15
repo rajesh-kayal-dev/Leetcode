9822fbd24b8f42f691c8323bb6db5a1287874128
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:01:25 +0530

    Create 206-ReverseLinkedList

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/206-ReverseLinkedList/206-ReverseLinkedList.js
@@ -0,0 +1,26 @@
+// Last updated: 6/15/2026, 10:01:24 AM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let current = head;
+
+    while(current !== null){
+        let next = current.next;
+
+        current.next = prev;
+
+        prev = current;
+        current = next;
+    }
+    return prev;
+};
