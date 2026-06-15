32d4ab2a55cf0cf9888eb1b9ee388f54d4ea775b
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:41 +0530

    Create 961-LongPressedName

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/961-LongPressedName/961-LongPressedName.java
@@ -0,0 +1,26 @@
+// Last updated: 6/15/2026, 9:59:40 AM
+class Solution {
+    public boolean isLongPressedName(String s1, String s2) {
+
+        int i = 0, j=0;
+        while(i < s1.length() && j <s2.length()){
+            char ch1= s1.charAt(i);
+            char ch2= s2.charAt(j);
+            if(ch1 == ch2){
+                i++;j++;
+            }else if(i > 0 && ch2==s1.charAt(i-1)){
+                j++;
+            }else{
+                return false;
+            }
+        }
+
+        if(i<s1.length()) return false;
+        
+        while(j<s2.length()){
+            if(s2.charAt(j) != s1.charAt(i-1)) return false;
+            j++;
+        }
+        return true;
+    }
+}
