60df5f85e2188626020cb704010c91514b7c2468
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:52 +0530

    Create 292-NimGame

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/292-NimGame/292-NimGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 6/15/2026, 10:00:51 AM
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4==0)
+        return 0;
+        else
+        return 1;
+        
+    }
+};
