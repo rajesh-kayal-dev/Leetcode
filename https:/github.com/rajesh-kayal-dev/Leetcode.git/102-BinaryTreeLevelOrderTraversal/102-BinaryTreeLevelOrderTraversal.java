9f752c1a74ef4da817b934965321266fd47677c1
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:02:25 +0530

    Create 102-BinaryTreeLevelOrderTraversal

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,43 @@
+// Last updated: 6/15/2026, 10:02:25 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if(root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int levelSize = q.size();
+
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0;i < levelSize;i++){
+                TreeNode current = q.poll();
+                level.add(current.val);
+
+                if(current.left != null) q.offer(current.left);
+                if(current.right != null) q.offer(current.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
