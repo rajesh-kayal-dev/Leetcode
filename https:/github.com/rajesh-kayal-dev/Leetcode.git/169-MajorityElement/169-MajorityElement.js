05f82508c5f25cdb5cd2c445949ed03b938d5314
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:01:41 +0530

    Create 169-MajorityElement

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/169-MajorityElement/169-MajorityElement.js
@@ -0,0 +1,22 @@
+// Last updated: 6/15/2026, 10:01:41 AM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+     let ans = nums[0];
+    let count = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (count === 0) {
+            ans = nums[i];
+            count = 1;
+        } else if (ans === nums[i]) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return ans;
+};
