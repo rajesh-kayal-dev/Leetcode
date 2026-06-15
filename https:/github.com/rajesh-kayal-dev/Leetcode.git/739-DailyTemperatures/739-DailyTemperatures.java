699b98db7a950ff670ed1b156c90552bb8dfde16
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:05 +0530

    Create 739-DailyTemperatures

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/739-DailyTemperatures/739-DailyTemperatures.java
@@ -0,0 +1,17 @@
+// Last updated: 6/15/2026, 10:00:05 AM
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        int n = temp.length;
+        int [] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n;i++){
+            while(!stack.isEmpty() && temp[i] > temp[stack.peek()]){
+                int index = stack.pop();
+                ans[index] = i - index;//day diffrence
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
