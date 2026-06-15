c7561d5577e7ba7a89195cb51a4fb80965a19801
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:22 +0530

    Create 567-PermutationInString

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/567-PermutationInString/567-PermutationInString.java
@@ -0,0 +1,25 @@
+// Last updated: 6/15/2026, 10:00:21 AM
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(s1Count, s2Count)) return true;
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            s2Count[s2.charAt(i) - 'a']++; // new char add
+            s2Count[s2.charAt(i - s1.length()) - 'a']--; // old char remove
+
+            if (Arrays.equals(s1Count, s2Count)) return true;
+        }
+
+        return false;
+    }
+}
