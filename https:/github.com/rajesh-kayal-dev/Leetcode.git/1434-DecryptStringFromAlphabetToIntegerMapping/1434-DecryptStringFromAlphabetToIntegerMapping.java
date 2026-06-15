e3647e3f139b5ced9643a3130afcf64ca4cf7878
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:29 +0530

    Create 1434-DecryptStringFromAlphabetToIntegerMapping

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/1434-DecryptStringFromAlphabetToIntegerMapping/1434-DecryptStringFromAlphabetToIntegerMapping.java
@@ -0,0 +1,23 @@
+// Last updated: 6/15/2026, 9:59:29 AM
+class Solution {
+    public String freqAlphabets(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int i =0;
+        while(i<n)
+        {
+            if((i+2)<n && s.charAt(i+2)=='#'){
+            String str=s.substring(i,i+2);
+            int temp =Integer.parseInt(str);
+            char ch=(char)(temp+96);
+            sb.append(ch);
+            i=i+3;
+            }else{
+                char ch = s.charAt(i);
+                sb.append((char)(ch+48));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
