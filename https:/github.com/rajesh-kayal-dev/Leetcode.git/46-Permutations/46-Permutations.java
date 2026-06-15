1aa3d66ca13dd774c4ea3b335ffed5cffb4a1964
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:03:00 +0530

    Create 46-Permutations

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/46-Permutations/46-Permutations.java
@@ -0,0 +1,25 @@
+// Last updated: 6/15/2026, 10:03:00 AM
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        backtrack(nums,current,result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current,List<List<Integer>> result){
+
+        if(current.size() == nums.length){
+            result.add(new ArrayList<>(current));//temp list - copy
+            return;
+        }
+
+        for(int i = 0; i < nums.length;i++){
+            if(current.contains(nums[i])) continue;
+
+            current.add(nums[i]);
+            backtrack(nums,current,result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
