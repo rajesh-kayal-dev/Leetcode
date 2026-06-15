c5068098abb74c3ec24d2b71ab121989003c999a
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:34 +0530

    Create 374-GuessNumberHigherOrLower

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/374-GuessNumberHigherOrLower/374-GuessNumberHigherOrLower.java
@@ -0,0 +1,30 @@
+// Last updated: 6/15/2026, 10:00:33 AM
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 0;
+        int high = n;
+
+        while(low <= high){
+            int mid= low + (high - low)/2;
+            int result = guess(mid);
+            
+            if(result == 0){
+                return mid; // Found the correct number
+            }else if(result < 0){
+                high = mid - 1; // Guess was too high
+            }else{
+                low = mid + 1; // Guess was too low
+            }
+        }
+        return -1; // Just in case, though problem guarantees it exists
+    }
+}
