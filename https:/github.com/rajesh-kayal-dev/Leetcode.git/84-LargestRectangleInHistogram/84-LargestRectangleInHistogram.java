f8da08308a5353394452b51ba20460d444047c09
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:02:31 +0530

    Create 84-LargestRectangleInHistogram

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/84-LargestRectangleInHistogram/84-LargestRectangleInHistogram.java
@@ -0,0 +1,27 @@
+// Last updated: 6/15/2026, 10:02:31 AM
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for(int i = 0 ; i <= n ; i++){
+            int currHeight = (i == n) ? 0 : heights[i];
+
+
+            while(!stack.isEmpty() && currHeight < heights[stack.peek()])
+            {
+                int top = stack.pop();
+                int height = heights[top];
+
+                int width = stack.isEmpty() ? i : i - stack.peek() -1;
+
+                int area = height * width;
+
+                maxArea = Math.max(maxArea, area);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
