2de2438c9df69bb46c26f5897039830b821c2b9f
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:02:20 +0530

    Create 110-BalancedBinaryTree

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -0,0 +1,35 @@
+// Last updated: 6/15/2026, 10:02:19 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+
+    private int dfsHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = dfsHeight(node.left);
+        if (left == -1) return -1;  // Left subtree unbalanced
+
+        int right = dfsHeight(node.right);
+        if (right == -1) return -1; // Right subtree unbalanced
+
+        if (Math.abs(left - right) > 1) return -1; // Current node unbalanced
+
+        return Math.max(left, right) + 1; // Return height
+    }
+}
