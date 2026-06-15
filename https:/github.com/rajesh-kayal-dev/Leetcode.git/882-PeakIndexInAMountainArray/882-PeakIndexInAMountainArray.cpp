13b37b519ec01683aff2af8ff6c15d42057f3467
Rajesh <rk6289943975@gmail.com>
2026-06-15 10:00:07 +0530

    Create 882-PeakIndexInAMountainArray

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/882-PeakIndexInAMountainArray/882-PeakIndexInAMountainArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/15/2026, 10:00:06 AM
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 0, end =arr.size()-1, mid;
+        while (start <= end) {
+            mid = end + (start - end) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+             start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return mid;
+    }
+};
