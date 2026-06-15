ff4a2decfc6a7d8e32b89a04c637c5a7c308e80c
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:25 +0530

    Create 1574-MaximumProductOfTwoElementsInAnArray

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/1574-MaximumProductOfTwoElementsInAnArray/1574-MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,19 @@
+// Last updated: 6/15/2026, 9:59:25 AM
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max =-1;
+        int smax=-1;
+
+        for(int i =0; i <nums.length;i++){
+
+            if(max < nums[i]){
+                smax = max;
+                max = nums[i];
+            }else if(smax < nums[i]){
+                smax = nums[i];
+            }
+        }
+        int ans = (max - 1) * (smax - 1);
+        return ans;
+    }
+}
