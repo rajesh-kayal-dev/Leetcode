c27645dba1381ede726816b3340f918d5d6f8802
Rajesh <rk6289943975@gmail.com>
2026-06-15 09:59:51 +0530

    Create 883-CarFleet

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/883-CarFleet/883-CarFleet.java
@@ -0,0 +1,29 @@
+// Last updated: 6/15/2026, 9:59:50 AM
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        double[][] cars = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            double time = (double)(target - position[i]) / speed[i];
+            cars[i][0] = position[i];
+            cars[i][1] = time;
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double prevTime = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            double currentTime = cars[i][1];
+
+            if (currentTime > prevTime) {
+                fleets++;
+                prevTime = currentTime;
+            }
+        }
+
+        return fleets;
+    }
+}
