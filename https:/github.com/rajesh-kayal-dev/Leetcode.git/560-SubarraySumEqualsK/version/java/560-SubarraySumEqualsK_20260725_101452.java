f683e839b446c628d62d58366767af9884f923ec
Rajesh <rk6289943975@gmail.com>
2026-07-25 10:14:53 +0530

    Create 560-SubarraySumEqualsK

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/560-SubarraySumEqualsK/version/java/560-SubarraySumEqualsK_20260725_101452.java
@@ -0,0 +1,17 @@
+// Last updated: 7/25/2026, 10:14:52 AM
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        int count = 0;
+4        int sum = 0;
+5        Map<Integer, Integer> map = new HashMap<>();
+6        map.put(0,1);
+7        for(int i = 0; i < nums.length; i++){
+8            sum += nums[i];
+9            if(map.containsKey(sum - k)){
+10                count += map.get(sum - k);
+11            }
+12            map.put(sum,map.getOrDefault(sum, 0) + 1);
+13        }
+14        return count;
+15    }
+16}
