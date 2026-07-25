4efe23738435851bac18bba68caa7e35f556510b
Rajesh <rk6289943975@gmail.com>
2026-07-25 10:15:39 +0530

    Create 141-LinkedListCycle

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/141-LinkedListCycle/141-LinkedListCycle.js
@@ -0,0 +1,27 @@
+// Last updated: 7/25/2026, 10:15:39 AM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+       let slow = head; 
+        let fast = head; 
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+};
