0bf8d78857c59e4372edaa891ec06ccee8488af6
Rajesh <rk6289943975@gmail.com>
2026-07-25 10:14:14 +0530

    Create 792-BinarySearch

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/792-BinarySearch/792-BinarySearch.js
@@ -0,0 +1,23 @@
+// Last updated: 7/25/2026, 10:14:13 AM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length -1;
+
+    while(low <= high){
+        let mid = Math.floor(low + (high - low) / 2);
+        if(nums[mid] === target){
+            return mid;
+        }else if(nums[mid] < target){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+};
