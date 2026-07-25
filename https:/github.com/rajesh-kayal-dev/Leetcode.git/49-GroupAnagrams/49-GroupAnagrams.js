37e2facddf29a33ec3fa84d05e6d26f296c26fe4
Rajesh <rk6289943975@gmail.com>
2026-07-25 10:59:42 +0530

    Create 49-GroupAnagrams

---
+++ https:/github.com/rajesh-kayal-dev/Leetcode.git/49-GroupAnagrams/49-GroupAnagrams.js
@@ -0,0 +1,20 @@
+// Last updated: 7/25/2026, 10:59:41 AM
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+
+    let map = {};
+
+    for(let word of strs){
+        let key = word.split('').sort().join('');
+
+        if(!map[key]){
+            map[key] = [];
+        }
+        map[key].push(word);
+    }
+    return Object.values(map)
+    
+};
